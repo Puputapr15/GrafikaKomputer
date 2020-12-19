3bfadb95e78e2c4d472b709134b5b9e163dd1f5a
Puputapr15 <76205901+Puputapr15@users.noreply.github.com>
2020-12-19 21:43:53 +0700

    Add files via upload

---
+++ Lesson04/Demo.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2, VBO3, VAO3, EBO3, texture3, VBO4, VAO4, EBO4, texture4, VBO5, VAO5, EBO5, texture5, VBO6, VAO6, EBO6, texture6;
+	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow *window);
+	void DrawColoredLemari();
+	void BuildColoredLemari();
+	void DrawColoredKulkas();
+	void BuildColoredKulkas();
+	void DrawColoredLaci();
+	void BuildColoredLaci();
+	void DrawColoredTV();
+	void BuildColoredTV();
+	void DrawColoredDinding();
+	void BuildColoredDinding();
+	void BuildColoredPlane();
+	void DrawColoredPlane();
+	void MoveCamera(float speed);
+	void StrafeCamera(float speed);
+	void RotateCamera(float speed);
+	void InitCamera();
+};
+
